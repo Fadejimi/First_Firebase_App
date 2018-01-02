f7d28e75e4b9f6709c7b56605c6af6892fcbdb33
Adegbulugbe Fadejimi <adegbulugbefadejimi@gmail.com>
2018-01-02 16:52:11 -0600

    Create user with realtime firebase database

--- app/src/main/java/com/fadejimi/savingsapp/MainActivity.java
+++ app/src/main/java/com/fadejimi/savingsapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.fadejimi.savingsapp;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +14,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.fadejimi.savingsapp.model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private EditText nameEditText;
+    private EditText emailEditText;
+    private Button saveButton;
+    private TextView txtDetails;
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
+
+    private String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +61,126 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        View appView = findViewById(R.id.app_view);
+        View contentView = appView.findViewById(R.id.content_view);
+
+        txtDetails = (TextView) contentView.findViewById(R.id.txt_user);
+        emailEditText = (EditText) contentView.findViewById(R.id.email);
+        nameEditText = (EditText) contentView.findViewById(R.id.name);
+        saveButton = (Button) contentView.findViewById(R.id.btn_save);
+
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+
+        // get reference to the users node
+        mFirebaseDatabase = mFirebaseInstance.getReference("users");
+
+        // store app to 'app-title' node
+        mFirebaseInstance.getReference("app_title").setValue("Realtime Database");
+
+        // app title change listener
+        mFirebaseInstance.getReference("app_title").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.e(TAG, "APP title updated");
+
+                String appTitle = dataSnapshot.getValue(String.class);
+
+                // update toolbar title
+                getSupportActionBar().setTitle(appTitle);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e(TAG, "Failed to read appTitle value", databaseError.toException());
+            }
+        });
+        saveButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        saveUser();
+                    }
+                }
+        );
+
+        toggleButton();
     }
 
+    private void saveUser() {
+        String name = nameEditText.getText().toString();
+        String email = emailEditText.getText().toString();
+
+        // check for already existed userId
+        if (TextUtils.isEmpty(userId)) {
+            createUser(name, email);
+        } else {
+            updateUser(name,email);
+        }
+    }
+
+    private void toggleButton() {
+        if (TextUtils.isEmpty(userId)) {
+            saveButton.setText("Save");
+        }
+        else {
+            saveButton.setText("Update");
+        }
+    }
+
+    private void createUser(String name, String email) {
+        // TODO
+        // In real apps userId should be fetched by using firebase Auth
+        if (TextUtils.isEmpty(userId)) {
+            userId = mFirebaseDatabase.push().getKey();
+        }
+
+        User user = new User(name, email);
+
+        mFirebaseDatabase.child(userId).setValue(user);
+
+        addUserChangeListener();
+    }
+
+    private void addUserChangeListener() {
+        mFirebaseDatabase.child(userId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+
+                // check for null
+                if (user == null) {
+                    Log.e(TAG, "User data is null");
+                    return;
+                }
+
+                Log.e(TAG, "User data is changed!" + user.name + ", " + user.email);
+
+                // Display newly updated name and email
+                txtDetails.setText(user.name + ", " + user.email);
+
+                emailEditText.setText("");
+                nameEditText.setText("");
+
+                toggleButton();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e(TAG, "Failed to read user", databaseError.toException());
+            }
+        });
+    }
+
+    private void updateUser(String name, String email) {
+        // updating the user via child nodes
+        if (!TextUtils.isEmpty(name)) {
+            mFirebaseDatabase.child(userId).child("name").setValue(name);
+        }
+        if (!TextUtils.isEmpty(email)) {
+            mFirebaseDatabase.child(userId).child("email").setValue(email);
+        }
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
---
+++ app/src/main/java/com/fadejimi/savingsapp/model/User.java
@@ -0,0 +1,19 @@
+package com.fadejimi.savingsapp.model;
+
+/**
+ * Created by Fadejimi on 1/2/18.
+ */
+
+public class User {
+    public String name;
+    public String email;
+
+    public User() {}
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+
+}
